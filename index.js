0d6906dc5105a8e26aab25c6a76ff338b646e0a1
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-03-12 13:13:18 +0100

    fix: remove cors

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const multer = require('multer');
-const cors = require('cors')
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
 
 app.use('/', express.static('content'));
 
